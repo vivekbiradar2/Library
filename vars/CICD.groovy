d0cdd64e861a28455d0c40c55fd214a1dca94042
vivekbiradar2 <118186581+vivekbiradar2@users.noreply.github.com>
2022-11-27 13:03:47 +0530

    Update CICD.groovy

--- vars/CICD.groovy
+++ vars/CICD.groovy
@@ -1,12 +1,5 @@
 Def newDownload(repo)
 {
- git 'https://github.com/intelliqittrainings/${repo}
+ git 'https://github.com/intelliqittrainings/${repo}'
 }
-Def newBuild()
-{
- sh 'mvn package'
-}
-Def newDeployment(IP,Name)
-{
- deploy adapters: [tomcat9(credentialsId: '107a6dfb-0bd5-41c4-a562-5477e7587c8a', path: '', url: 'http://${IP}:8080')], contextPath: '${Name}', war: '**/*.war'
-}
+
